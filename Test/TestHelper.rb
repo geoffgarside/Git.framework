7a108f02b4deb3f1ce87ae63e017d6c45839322e
Geoff Garside <geoff@geoffgarside.co.uk>
2009-09-17 23:59:50 +0100

    Couple amendments to the TestHelper.rb file

--- Test/TestHelper.rb
+++ Test/TestHelper.rb
@@ -8,7 +8,7 @@ append_framework_path File.join(File.dirname(__FILE__) +'/../build/Debug')
 require 'bacon'
 framework 'Git'
 
-Bacon.extend Bacon::TestUnitOutput
+Bacon.extend Bacon::TestUnitOutput  # This doesn't seem to work at the moment, but its how we'd prefer it
 Bacon.summary_on_exit
 
 # hdiutil create -volname "Git Test Repos" -fs HFS+J -layout GPTSPUD -size 2m Test/Resources/repos.dmg
@@ -17,7 +17,7 @@ TEST_VOLUME = "/Volumes/Git Test Repos"
 TEST_REPO = File.join(TEST_VOLUME, 'Test')
 TEST_REPO_ROOT = "#{TEST_REPO}/.git"
 
-unless File.exist? TEST_REPO
+unless File.exist? TEST_VOLUME
   puts 'Mounting git repository disk image'
   `hdiutil attach #{File.join(File.dirname(__FILE__), 'Resource/repos.dmg')}`
 end
