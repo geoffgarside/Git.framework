5a00d84e1bfd8e29b455a1b1aee6d1312fb6e54c
Geoff Garside <geoff@geoffgarside.co.uk>
2009-11-28 22:09:48 +0000

    Add documentation for GITBranch

--- Source/GITBranch.h
+++ Source/GITBranch.h
@@ -10,6 +10,10 @@
 
 
 @class GITRepo, GITRef;
+
+/*!
+ * \c GITBranch objects provide an interface to branches.
+ */
 @interface GITBranch : NSObject {
     GITRepo *repo;
     GITRef *ref;
@@ -18,11 +22,44 @@
 @property (retain) GITRepo *repo;
 @property (retain) GITRef *ref;
 
+/// \name Creating and Initialising Branches
+/*!
+ * Creates and returns a branch by the name specified.
+ *
+ * The name of the branch is used to resolve the reference for the branch,
+ * the resolved reference is used to create the GITBranch.
+ *
+ * \param theName Name of the branch
+ * \param theRepo Repository to resolve the branch in
+ * \return branch initialised with the name specified, or nil if the branch can't be resolved
+ * \sa branchFromRef:
+ * \sa initFromRef:
+ */
 + (GITBranch *)branchWithName: (NSString *)theName inRepo: (GITRepo *)theRepo;
+
+/*!
+ * Creates and returns an autoreleased branch using the reference specified￼.
+ *
+ * \param theRef GITRef describing the branch
+ * \return branch initialised with the reference
+ * \sa initFromRef:
+ */
 + (GITBranch *)branchFromRef: (GITRef *)theRef;
 
+/*!
+ * Creates and returns a branch for the specified reference￼.
+ *
+ * \param theRef GITRef describing the branch
+ * \return branch initialised with the reference
+ */
 - (id)initFromRef: (GITRef *)theRef;
 
+/// \name Branch Information
+/*!
+ * Returns the name of the branch￼.
+ *
+ * \return name of the branch
+ */
 - (NSString *)name;
 
 @end
