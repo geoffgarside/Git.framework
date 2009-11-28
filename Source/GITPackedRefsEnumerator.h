65c27c7c96739b68583f94e97aebdc0caeed3c26
Geoff Garside <geoff@geoffgarside.co.uk>
2009-11-28 23:43:40 +0000

    Document GITPackedRefsEnumerator

--- Source/GITPackedRefsEnumerator.h
+++ Source/GITPackedRefsEnumerator.h
@@ -9,6 +9,34 @@
 #import <Foundation/NSEnumerator.h>
 
 @class NSData, NSString, GITRepo;
+
+/*!
+ * The \c GITPackedRefsEnumerator class provides enumeration
+ * of \c packed-refs contents.
+ *
+ * This enumerator allows for iteration over the contents of
+ * \c packed-refs using either normal enumeration
+ *
+\verbatim
+id ref;
+GITRepo *repo = [GITRepo repo];
+GITPackedRefsEnumerator *e = [GITPackedRefsEnumerator enumeratorForRepo:repo];
+
+while ( (object = [e nextObject]) ) {
+    // do something with object...
+}
+\endverbatim
+ *
+ * or using Fast Enumeration
+ *
+\verbatim
+GITRepo *repo = [GITRepo repo];
+
+for ( id object in [GITPackedRefsEnumerator enumeratorForRepo:repo]) {
+    // do something with object...
+}
+\endverbatim
+ */
 @interface GITPackedRefsEnumerator : NSEnumerator {
     NSString *packedRefsPath;
     BOOL hasPackedRefs;
@@ -18,7 +46,22 @@
     NSRange currentRange;
 }
 
+//! \name Creating and Initialising Packed Refs Enumerator
+/*!
+ * Returns a packed refs enumerator for the provided repository￼.
+ *
+ * \param theRepo Repository to create the enumerator for
+ * \return packed refs enumerator
+ * \sa initWithRepo:
+ */
 + (GITPackedRefsEnumerator *)enumeratorForRepo: (GITRepo *)theRepo;
+
+/*!
+ * Returns a packed refs enumerator for the provided repository￼.
+ *
+ * \param theRepo Repository to create the enumerator for
+ * \return packed refs enumerator
+ */
 - (id)initWithRepo: (GITRepo *)theRepo;
 
 @end
