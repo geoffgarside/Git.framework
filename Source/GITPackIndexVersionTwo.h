f422a6a1783a1a6a521f97911691c539b1418f12
Geoff Garside <geoff@geoffgarside.co.uk>
2009-12-06 22:27:15 +0000

    Add proper support to GITPackIndexVersionTwo class

--- Source/GITPackIndexVersionTwo.h
+++ Source/GITPackIndexVersionTwo.h
@@ -11,9 +11,11 @@
 
 @interface GITPackIndexVersionTwo : GITPackIndex {
     NSData *data;
+    NSArray *fanoutTable;
 }
 
 @property (copy) NSData *data;
+@property (copy) NSArray *fanoutTable;
 
 /*!
  * Returns the index of the SHA1 contained in \a packedSha.
@@ -24,5 +26,47 @@
  * \sa indexOfSha1:
  */
 - (NSUInteger)indexOfPackedSha1: (NSData *)packedSha;
+
+/*!
+ * Returns the offset value from the position in the offset table at the specified \a idx.
+ *
+ * \param idx Index position of the offset to retrieve
+ * \return the offset at the specified index position
+ * \sa extendedPackOffsetAtOffset:
+ * \sa packOffsetForSha1:error:
+ * \sa packOffsetForSha1:
+ */
+- (off_t)packOffsetAtIndex: (NSUInteger)idx;
+
+/*!
+ * Returns the offset value from the position specified in the extended offset table at the specified \a idx.
+ *
+ * \param idx Index position of the offset to retrieve
+ * \return the offset at the specified index position
+ * \sa packOffsetAtIndex:
+ * \sa packOffsetForSha1:error:
+ * \sa packOffsetForSha1:
+ */
+- (off_t)extendedPackOffsetAtOffset: (uint32_t)idx;
+
+/*!
+ * Returns the offset in the PACK file of the object \a packedSha.
+ *
+ * \param packedSha SHA1 of the object to get the offset of
+ * \param error NSError describing the error which occurred
+ * \return offset in the PACK of the object \a packedSha or NSNotFound if an error occurred
+ * \sa packOffsetForSha1:error:
+ * \sa packOffsetForSha1:
+ * \sa packOffsetAtIndex:
+ */
+- (off_t)packOffsetForPackedSha1: (NSData *)packedSha error: (NSError **)error;
+
+- (NSRange)fanoutTableRange;
+- (NSRange)shaTableRange;
+- (NSRange)crcTableRange;
+- (NSRange)offsetTableRange;
+- (NSRange)extendedOffsetTableRange;
+- (NSRange)packChecksumRange;
+- (NSRange)indexChecksumRange;
 
 @end
--- Source/GITPackIndexVersionTwo.m
+++ Source/GITPackIndexVersionTwo.m
@@ -8,11 +8,25 @@
 
 #import "GITPackIndexVersionTwo.h"
 #import "GITObjectHash.h"
+#import "GITError.h"
+#import "NSRangeEnd.h"
 
+
+#define EXTENDED_OFFSET_FLAG (1 << 31)
+
+static const short _fanOutStart     = 8;    //!< Bytes
+static const short _fanOutSize      = 4;    //!< Bytes
+static const short _fanOutCount     = 256;  //!< Number of entries
+static const short _fanOutEnd       = 1032; //!< Start + (Size * Count)
+static const short _fanOutEntrySize = 24;   //!< Bytes
+
+static const short _crcSize         = 4;    //!< Bytes
+static const short _offsetSize      = 4;    //!< Bytes
+static const short _extOffsetSize   = 8;    //!< Bytes
 
 @implementation GITPackIndexVersionTwo
 
-@synthesize data;
+@synthesize data, fanoutTable;
 
 - (NSUInteger)version {
     return 2;
@@ -33,15 +47,60 @@
     [super dealloc];
 }
 
+- (BOOL)parseFanoutTable: (NSError **)error {
+    uint32_t numberOfObjects = 0;
+    NSUInteger i, last, current;
+    NSMutableArray *newFanoutTable = [NSMutableArray arrayWithCapacity:_fanOutCount];
+    NSRange fanoutRange = [self fanoutTableRange];
+
+    last = current = 0;
+    for ( i = 0; i < _fanOutCount; i++ ) {
+        NSRange range = NSMakeRange(i * _fanOutSize + fanoutRange.location, _fanOutSize);
+        [data getBytes:&numberOfObjects range:range];
+        current = CFSwapInt32BigToHost(numberOfObjects);
+
+        if ( last > current ) {
+            GITError(error, GITPackIndexErrorCorrupt, NSLocalizedString(@"PACK Index is corrupt, invalid fan out table", @"GITPackIndexErrorCorrupt"));
+            return NO;
+        }
+
+        [newFanoutTable addObject:[NSNumber numberWithUnsignedInteger:current]];
+        last = current;
+    }
+
+    self.fanoutTable = [newFanoutTable copy];
+    return YES;
+}
+
+/*!
+ * We have to copy this from GITPackIndex as the @synthesize statement above kills it
+ */
+- (NSArray *)fanoutTable {
+    return [self fanoutTable:NULL];
+}
+
+- (NSArray *)fanoutTable: (NSError **)error {
+    if ( !fanoutTable ) {
+        if ( ![self parseFanoutTable:error] )
+            return nil;
+    }
+    return fanoutTable;
+}
+
+- (NSUInteger)indexOfSha1: (GITObjectHash *)objectHash {
+    return [self indexOfPackedSha1:[objectHash packedData]];
+}
+
 - (NSUInteger)indexOfPackedSha1: (NSData *)packedSha {
     uint8_t *packedShaBytes = (uint8_t*)[packedSha bytes];
     uint8_t *indexDataBytes = (uint8_t*)[self.data bytes];
+    NSRange shaTable = [self shaTableRange];
 
     GITFanoutEntry fanoutEntry = [self fanoutEntryForShasStartingWithByte:packedShaBytes[0]];
     if ( fanoutEntry.numberOfEntries > 0 ) {
         NSUInteger lo = fanoutEntry.numberOfPriorEntries;
         NSUInteger hi = lo + fanoutEntry.numberOfEntries;
-        NSUInteger loc = 0;    // [self rangeOfSHATable].location;
+        NSUInteger loc = shaTable.location;
 
         do {
             NSUInteger mid = (lo + hi) >> 1;    // divide by 2 ;)
@@ -54,6 +113,88 @@
     }
 
     return NSNotFound;
+}
+
+- (off_t)packOffsetAtIndex: (NSUInteger)idx {
+    // Get bytes for offset at position in OffsetTable
+    // If bytes for offset have MSB set then parse ExtOffsetTable offset value
+    NSRange offsets = [self offsetTableRange];
+    NSUInteger pos  = idx * _offsetSize;
+
+    if ( pos >= offsets.length ) {
+        [NSException raise:NSRangeException format:@"PACK Index Entry %u (offset:%lu) out of bounds (%@)",
+             idx, pos, NSStringFromRange(offsets)];
+    }
+
+    uint32_t offset;
+    [self.data getBytes:&offset range:NSMakeRange(offsets.location + pos, _offsetSize)];
+
+    offset = CFSwapInt32BigToHost(offset);
+    if ( offset & EXTENDED_OFFSET_FLAG )
+        return [self extendedPackOffsetAtOffset:offset];
+    return (off_t)offset;
+}
+
+- (off_t)extendedPackOffsetAtOffset: (uint32_t)idx {
+    NSRange offsets = [self extendedOffsetTableRange];
+    uint32_t pos    = (idx & ~EXTENDED_OFFSET_FLAG) * _extOffsetSize;
+
+    if ( pos >= offsets.length ) {
+        [NSException raise:NSRangeException format:@"Extended PACK Index Entry from %u (offset:%lu) out of bounds (%@)",
+             idx, pos, NSStringFromRange(offsets)];
+    }
+
+    uint64_t offset;
+    [self.data getBytes:&offset range:NSMakeRange(offsets.location + pos, _extOffsetSize)];
+
+    return (off_t)CFSwapInt64BigToHost(offset);
+}
+
+- (off_t)packOffsetForSha1: (GITObjectHash *)objectHash error: (NSError **)error {
+    return [self packOffsetForPackedSha1:[objectHash packedData] error:error];
+}
+
+- (off_t)packOffsetForPackedSha1: (NSData *)packedSha error: (NSError **)error {
+    NSUInteger idx = [self indexOfPackedSha1:packedSha];
+    if ( idx != NSNotFound )
+        return [self packOffsetAtIndex:idx];
+
+    GITError(error, GITPackErrorObjectNotFound, NSLocalizedStringWithArguments(@"Object <%@> not found in PACK Index file", @"GITPackErrorObjectNotFound", [GITObjectHash unpackedStringFromData:packedSha]));
+    return NSNotFound;
+}
+
+//! Ignore _fanoutEnd incase we stuff that up
+- (NSRange)fanoutTableRange {
+    return NSMakeRange(_fanOutStart, _fanOutSize * _fanOutCount);
+}
+
+- (NSRange)shaTableRange {
+    NSUInteger endTable = NSRangeEnd([self fanoutTableRange]);
+    return NSMakeRange(endTable, GITObjectHashPackedLength * [self numberOfObjects]);
+}
+
+- (NSRange)crcTableRange {
+    NSUInteger endTable = NSRangeEnd([self shaTableRange]);
+    return NSMakeRange(endTable, _crcSize * [self numberOfObjects]);
+}
+
+- (NSRange)offsetTableRange {
+    NSUInteger endTable = NSRangeEnd([self crcTableRange]);
+    return NSMakeRange(endTable, _offsetSize * [self numberOfObjects]);
+}
+
+- (NSRange)extendedOffsetTableRange {
+    NSUInteger endTable = NSRangeEnd([self offsetTableRange]);
+    NSUInteger length   = [self.data length] - endTable - (2 * GITObjectHashPackedLength);
+    return NSMakeRange(endTable, length);
+}
+
+- (NSRange)packChecksumRange {
+    return NSMakeRange([self.data length] - (2 * GITObjectHashPackedLength), GITObjectHashPackedLength);
+}
+
+- (NSRange)indexChecksumRange {
+    return NSMakeRange([self.data length] - GITObjectHashPackedLength, GITObjectHashPackedLength);
 }
 
 @end
