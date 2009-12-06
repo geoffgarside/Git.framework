364ecc939784fcce342dded485e3f9e4eb2f9909
Geoff Garside <geoff@geoffgarside.co.uk>
2009-12-06 22:45:47 +0000

    Change GITPackFilePlaceholder -initWithData:indexPath:error: method to use uint32_t type to read signature and version into.
    This helps with the error message should the version not be supported as we can print a more meaningful error message.

--- Source/GITPackFilePlaceholder.m
+++ Source/GITPackFilePlaceholder.m
@@ -40,29 +40,29 @@ static const uint8_t const GITPackFileVersionTwoBytes[] = { 0x0, 0x0, 0x0, 0x2 }
     if ( !packData )
         return nil; // No error explanation, seems a bit silly to say "Oi!! packData was nil."
     
-    uint8_t buf[4]; // to read file signature and version into
+    uint32_t num, *n = &num; // to read file signature and version into
     NSZone *z = [self zone]; [self release];
     
     // Extract signature bytes
-    memset(buf, 0x0, 4);
-    [packData getBytes:buf range:NSMakeRange(0, 4)];
+    memset(n, 0x0, 4);
+    [packData getBytes:n range:NSMakeRange(0, 4)];
     
-    if ( memcmp(buf, GITPackFileSignature, 4) != 0 ) { // signature doesn't match, not a pack file
+    if ( memcmp(n, GITPackFileSignature, 4) != 0 ) { // signature doesn't match, not a pack file
         GITError(error, GITPackFileErrorFileIsInvalid, NSLocalizedString(@"PACK Data is missing signature", @"GITPackFileErrorFileIsInvalid"));
         return nil;
     }
     
     // Extract version bytes
-    memset(buf, 0x0, 4);
-    [packData getBytes:buf range:NSMakeRange(4, 4)];
+    memset(n, 0x0, 4);
+    [packData getBytes:n range:NSMakeRange(4, 4)];
     
     // Initialise version specific class
-    if ( memcmp(buf, GITPackFileVersionTwoBytes, 4) == 0 ) { // Version 2 PACK file
+    if ( memcmp(n, GITPackFileVersionTwoBytes, 4) == 0 ) { // Version 2 PACK file
         return [[GITPackFileVersionTwo allocWithZone:z] initWithData:packData indexPath:indexPath error:error];
     }
     
     // Raise error as version not supported
-    NSString *desc = [NSString stringWithFormat:NSLocalizedString(@"PACK file version %d%d%d%d unsupported", @"GITPackFileErrorVersionUnsupported"), buf[0], buf[1], buf[2], buf[3]];
+    NSString *desc = [NSString stringWithFormat:NSLocalizedString(@"PACK file version %u unsupported", @"GITPackFileErrorVersionUnsupported"), CFSwapInt32BigToHost(num)];
     GITError(error, GITPackFileErrorVersionUnsupported, desc);
     return nil;
 }
