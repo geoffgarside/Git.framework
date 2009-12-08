73ea7ec6b66e01a2325b6be380ecd25a90b6e473
Geoff Garside <geoff@geoffgarside.co.uk>
2009-12-08 22:57:38 +0000

    Add NSData (Compression) category to add ZLib Compression and Decompression support to NSData

---
+++ Source/Category/NSData/NSData+Compression.h
@@ -0,0 +1,41 @@
+//
+//  NSData+Compression.h
+//
+//  Created by Geoffrey Garside on 29/06/2008.
+//  Believed to be Public Domain.
+//
+//  Methods extracted from source given at
+//  http://www.cocoadev.com/index.pl?NSDataCategory
+//
+
+#import <Foundation/NSData.h>
+
+/*! Adds compression and decompression messages to NSData.
+ * Methods extracted from source given at
+ * http://www.cocoadev.com/index.pl?NSDataCategory
+ */
+@interface NSData (Compression)
+
+#pragma mark -
+#pragma mark Zlib Compression routines
+/*! Returns a data object containing a Zlib decompressed copy of the receivers contents.
+ * \returns A data object containing a Zlib decompressed copy of the receivers contents.
+ */
+- (NSData *) zlibInflate;
+/*! Returns a data object containing a Zlib compressed copy of the receivers contents.
+ * \returns A data object containing a Zlib compressed copy of the receivers contents.
+ */
+- (NSData *) zlibDeflate;
+
+#pragma mark -
+#pragma mark Gzip Compression routines
+/*! Returns a data object containing a Gzip decompressed copy of the receivers contents.
+ * \returns A data object containing a Gzip decompressed copy of the receivers contents.
+ */
+- (NSData *) gzipInflate;
+/*! Returns a data object containing a Gzip compressed copy of the receivers contents.
+ * \returns A data object containing a Gzip compressed copy of the receivers contents.
+ */
+- (NSData *) gzipDeflate;
+
+@end
