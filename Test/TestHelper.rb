33e560fedf33c1bedadfe79b28bb6e41f46e41c0
Geoff Garside <geoff@geoffgarside.co.uk>
2009-12-09 22:24:58 +0000

    Remove a lot of cruft from the TestHelper now we have BridgeSupport

--- Test/TestHelper.rb
+++ Test/TestHelper.rb
@@ -2,28 +2,6 @@ def default_repository
   GITRepo.repoWithRoot(TEST_REPO)
 end
 
-GITRepoErrorRootDoesNotExist    = -1
-GITRepoErrorRootNotAccessible   = -2
-GITRepoErrorRootInsane          = -3
-GITRefResolverErrorRefNotFound  = -4
-
-class GITRef
-  def link?
-    self.isLink == 1
-  end
-end
-
-# Useful string encodings
-NSUTF8StringEncoding = 4
-
-NSNotFound = 0x7fffffffffffffff   # NSNotFound = NSIntegerMax = 0x7fffffffffffffff
-
-class NSObject
-  def ==(rhs)
-    self.isEqual(rhs) ? true : false
-  end
-end
-
 class NSData
   def ===(rhs)
     raise ArgumentError, "Must be kind of NSData" unless rhs.kind_of?(NSData)
