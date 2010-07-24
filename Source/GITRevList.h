1a8cdc31c7df13081f9b872fff06591f4245f3be
Geoff Garside <geoff@geoffgarside.co.uk>
2010-07-24 11:47:11 +0100

    Add documentation to GITRevList

--- Source/GITRevList.h
+++ Source/GITRevList.h
@@ -10,18 +10,67 @@
 
 
 @class GITCommit, GITGraph;
+
+/*!
+ * The GITRevList class lists commit objects in reverse chronological order. A subset
+ * of the commit history can be excluded or negated from the list by calling -subtractDescendentsFromCommit:
+ * which will remove all commits reachable from the specified commit from the list.
+ *
+ * Sorted lists of commits are provided through one of the <tt>-arrayOfCommitsSortedBy*</tt> methods.
+ */
 @interface GITRevList : NSObject {
     GITGraph *graph;
 }
 
+//! \name Creating and Initialising Rev-Lists
+/*!
+ * Creates and returns a rev list object with the history from the \a head commit.
+ *
+ * \param head Commit object to list from
+ * \return New rev list object with the history of the \a head commit.
+ */
 + (GITRevList *)revListWithCommit: (GITCommit *)head;
 
+/*!
+ * Initializes and returns a rev list object with history from the \a head commit.
+ *
+ * \param head Commit object to list from
+ * \return Rev list object with the history of the \a head commit.
+ */
 - (id)initWithCommit: (GITCommit *)head;
 
+//! \name Modifying Rev-Lists
+/*!
+ * Removes commits from the receiver which are in the history of the given \a tail commit.
+ */
 - (void)subtractDescendentsFromCommit: (GITCommit *)tail;
 
+//! \name Sorted Lists from Rev-Lists
+/*!
+ * Returns an array of commit objects sorted by their commit date.
+ *
+ * The result of this method is equivalent to <tt>git rev-list</tt>.
+ *
+ * \return array of commits sorted by date
+ */
 - (NSArray *)arrayOfCommitsSortedByDate;
+
+/*!
+ * Returns an array of commit objects sorted by topology.
+ *
+ * The result of this method is equivalent to <tt>git rev-list --topo-order</tt>.
+ *
+ * \return array of commits sorted by topology
+ */
 - (NSArray *)arrayOfCommitsSortedByTopology;
+
+/*!
+ * Returns an array of commit objects sorted by topology and commit date.
+ *
+ * The result of this method is equivalent to <tt>git rev-list --date-order</tt>.
+ *
+ * \return array of commits sorted by topology and date
+ */
 - (NSArray *)arrayOfCommitsSortedByTopologyAndDate;
 
 @end
