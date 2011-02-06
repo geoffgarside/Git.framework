8006e6428172f0bd6a90abbc6c259e798d23d77b
Geoff Garside <geoff@geoffgarside.co.uk>
2011-02-06 11:34:10 +0000

    Add class cluster support methods to GITPackIndexWriter

--- Source/GITPackIndexWriter.h
+++ Source/GITPackIndexWriter.h
@@ -9,8 +9,9 @@
 #import <Foundation/Foundation.h>
 
 
-@interface GITPackIndexWriter : NSObject {
-
-}
+/*!
+ * A class cluster
+ */
+@interface GITPackIndexWriter : NSObject { }
 
 @end
--- Source/GITPackIndexWriter.m
+++ Source/GITPackIndexWriter.m
@@ -7,8 +7,25 @@
 //
 
 #import "GITPackIndexWriter.h"
+#import "GITPackIndexWriterPlaceholder.h"
 
 
 @implementation GITPackIndexWriter
+
++ (id)alloc {
+    if ([self isEqual:[GITPackIndexWriter class]])
+        return [GITPackIndexWriterPlaceholder alloc];
+    else return [super alloc];
+}
+
++ (id)allocWithZone: (NSZone *)zone {
+    if ([self isEqual:[GITPackIndexWriter class]])
+        return [GITPackIndexWriterPlaceholder allocWithZone:zone];
+    else return [super allocWithZone:zone];
+}
+
+- (id)copyWithZone: (NSZone *)zone {
+    return self;
+}
 
 @end
