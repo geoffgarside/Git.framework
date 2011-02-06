675547575dbc3ebc15171e1ef99160eba53a468a
Geoff Garside <geoff@geoffgarside.co.uk>
2011-02-06 12:50:03 +0000

    Correct GITPackFileWriter tests to match method signature

--- Test/Unit/GITPackFileWriter.rb
+++ Test/Unit/GITPackFileWriter.rb
@@ -1,12 +1,8 @@
 describe "GITPackFileWriter" do
   before do
-    @err  = Pointer.new(:object)
-    @writer = GITPackFileWriter.packWriterVersion(2, error:@err)
+    @writer = GITPackFileWriter.packWriterVersion(2)
   end
   should 'not be nil' do
     @writer.should.not.be.nil
-  end
-  should 'not give an error' do
-    @err[0].should.be.nil
   end
 end
