3e14af3ce5e955d7148f203c7b983d4f9af58b11
Geoff Garside <geoff@geoffgarside.co.uk>
2011-02-07 00:50:55 +0000

    Add GITPackIndexWriter Shared methods.
    These methods are shared between the two versions of Index file we currently generate.

---
+++ Source/GITPackIndexWriter+Shared.h
@@ -0,0 +1,18 @@
+//
+//  GITPackIndexWriter+Shared.h
+//  Git.framework
+//
+//  Created by Geoff Garside on 06/02/2011.
+//  Copyright 2011 Geoff Garside. All rights reserved.
+//
+
+#import "GITPackIndexWriter.h"
+
+
+@class GITObjectHash;
+@interface GITPackIndexWriter (Shared)
+
+- (void)addObjectHashToFanoutTable: (GITObjectHash *)sha1;
+- (NSInteger)writeFanoutTableToStream: (NSOutputStream *)stream;
+
+@end
